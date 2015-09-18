f0d7a4bd1f7b0fc99a0df6ef94faf2d5a0e75d4a
bhaskarchatterjee28 <bhaskarchatterjee28@gmail.com>
2015-09-18 23:33:17 +0530

    changes

--- js/bin/app.js
+++ js/bin/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function(){
     $('.slider').slider({
         full_width: true,
@@ -13,7 +14,7 @@ $(document).ready(function(){
         outDuration: 225,
         constrain_width: false,
         hover: true,
-        gutter: 2,
+        gutter: 1,
         belowOrigin: true,
         alignment: 'left'
     });
@@ -23,3 +24,21 @@ $('ul#HeadBar').children().click(function(){
     $(this).addClass('active');
     $(this).parents('#HeadBar').children().not(this).removeClass('active');
 });
+=======
+    $(document).ready(function(){
+        $('.slider').slider({full_width: true});
+	    $('.slider').slider({height:500});
+	    $('ul.tabs').tabs();
+        $(".button-collapse").sideNav();
+        $('.dropdown-button').dropdown({
+                inDuration: 300,
+                outDuration: 225,
+                constrain_width: false, // Does not change width of dropdown to that of the activator
+                hover: true, // Activate on hover
+                gutter: 2, // Spacing from edge
+                belowOrigin: true, // Displays dropdown below the button
+                alignment: 'right' // Displays dropdown with edge aligned to the left of button
+            }
+        );
+    });
+>>>>>>> parent of 2554f5b... Minor Changes
